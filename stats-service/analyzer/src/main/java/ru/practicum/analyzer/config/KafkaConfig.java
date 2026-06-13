97994c8d32eb194fde6fff7d037f004a6cfd9987
Kirill-Kalinin-2025 <kirya.kalina.06@yandex.ru>
2026-06-13 22:12:13 +0300

    Add recommendations. Part 17.

--- stats-service/analyzer/src/main/java/ru/practicum/analyzer/config/KafkaConfig.java
+++ stats-service/analyzer/src/main/java/ru/practicum/analyzer/config/KafkaConfig.java
@@ -21,7 +21,7 @@ public class KafkaConfig {
     @Value("${spring.kafka.bootstrap-servers:localhost:9092}")
     private String bootstrapServers;
 
-    @Value("${spring.kafka.consumer.properties.schema.registry.url:http://localhost:8082}")
+    @Value("${spring.kafka.consumer.properties.schema.registry.url:http://localhost:8081}")
     private String schemaRegistryUrl;
 
     @Bean
