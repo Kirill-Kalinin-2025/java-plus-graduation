e132df49ab064166e6c125bc7f06d49484a1baa8
Kirill-Kalinin-2025 <kirya.kalina.06@yandex.ru>
2026-06-14 00:00:36 +0300

    Add recommendations. Part 24.

--- stats-service/aggregator/src/main/java/ru/practicum/aggregator/KafkaConfig.java
+++ stats-service/aggregator/src/main/java/ru/practicum/aggregator/KafkaConfig.java
@@ -29,7 +29,7 @@ public class KafkaConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, "aggregator");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, ByteArrayDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, true);
         return new DefaultKafkaConsumerFactory<>(props);
     }
